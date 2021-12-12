908ad726bb24eb2dc3c2c376d19df592c3c3578c
TimothyJan <jan.timothy@gmail.com>
2021-12-11 17:23:56 -0800

    Initial Commit

---
+++ SwiftNotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftNotesApp
+//
+//  Created by Timothy Jan on 12/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
